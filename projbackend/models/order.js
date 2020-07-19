0e95bcd15c9a0fee4c223977a43fc2eadedbc5fd
Vineeta Pathak <pathak.vineeta786@gmail.com>
2020-07-19 14:10:39 +0530

    updated code

--- projbackend/models/order.js
+++ projbackend/models/order.js
@@ -1,2 +1,30 @@
 const mongoose = require("mongoose");
-const schema = mongoose.Schema;
+const { ObjectId } = mongoose.Schema;
+var schema = mongoose.Schema;
+
+const ProductCartSchema = new schema({
+  product: {
+    type: ObjectId,
+    ref: "Product",
+  },
+  name: String,
+  count: Number,
+  price: Number,
+});
+
+const OrderSchema = new schema(
+  {
+    products: [ProductCartSchema],
+    transaction_id: {},
+    amount: {
+      type: Number,
+    },
+    address: String,
+    updated: Date,
+    user: {
+      type: ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
--- projbackend/models/product.js
+++ projbackend/models/product.js
@@ -43,3 +43,4 @@ const productSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+module.exports = mongoose.model("Product", productSchema);
