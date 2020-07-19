d5009570424b1db9f9be9649f42ba9791d8b3524
Vineeta Pathak <pathak.vineeta786@gmail.com>
2020-07-19 14:46:04 +0530

    updated code

--- projbackend/app.js
+++ projbackend/app.js
@@ -1,19 +1,21 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
-mongoose.connect("mongodb://localhost:27017/myapp");
+require("dotenv").config();
+
 mongoose
-  .connect("mongodb://localhost:27017/tshirt", {
+  .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
   })
   .then(() => {
     console.log("DB Connected");
-  });
+  })
+  .catch(console.log("Not connected!"));
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-  console.log(`app is running at${port}`);
+  console.log(`app is running at ${port}`);
 });
