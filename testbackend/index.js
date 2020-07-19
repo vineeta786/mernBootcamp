dc9a7eb0706a3a62de186f0ac1a830331486a3bf
Vineeta Pathak <pathak.vineeta786@gmail.com>
2020-07-19 16:06:09 +0530

    updated code

--- testbackend/index.js
+++ testbackend/index.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const app = express();
-const port = 8000;
+const port = 7000;
 
 app.get("/", (req, res) => {
   return res.send("hello there!!!");
 });
-
+const admin = (req, res) => {
+  return res.send("this is admin!");
+};
+const isAdmin = (req, res, next) => {
+  console.log("isAdmin is running!!");
+  next();
+};
+const isloggedIn = (req, res, next) => {
+  console.log("isloggedIn is running!");
+  next();
+};
+app.get("/admin", isloggedIn, isAdmin, admin);
 app.get("/signup", (req, res) => {
   return res.send("on the signUp page !!!");
 });
